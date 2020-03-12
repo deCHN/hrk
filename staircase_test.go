c3c1a6ffd6e6c62789f6fb884ad2e5f14563051a
deChn <sehr.jimmy@gmail.com>
2020-03-12 21:32:26 +0100

    Add staircase 10/10.

---
+++ staircase_test.go
@@ -0,0 +1,23 @@
+package hrk
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestStaircase(t *testing.T) {
+	staircase(10)
+}
+
+// Complete the plusMinus function below.
+func staircase(n int32) {
+	for i := 1; i <= int(n); i++ {
+		fmt.Println(fixedLengthString(int(n), strings.Repeat("#", i)))
+	}
+}
+
+func fixedLengthString(length int, str string) string {
+	verb := fmt.Sprintf("%%%d.%ds", length, length)
+	return fmt.Sprintf(verb, str)
+}
