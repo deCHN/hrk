3468e5a482cba5b42b1643a54017ad00abe2de37
deChn <hey.ceth@gmail.com>
2020-03-23 23:34:23 +0100

    Add test breaking-records.

---
+++ breakingRecords_test.go
@@ -0,0 +1,25 @@
+package hrk
+
+import "testing"
+
+func TestBreakingRecords(t *testing.T) {
+	input := [][]int32{
+		[]int32{10, 5, 20, 20, 4, 5, 2, 25, 1},
+		[]int32{3, 4, 21, 36, 10, 28, 35, 5, 24, 42},
+	}
+
+	expect := [][2]int32{
+		[2]int32{2, 4},
+		[2]int32{4, 0},
+	}
+
+	for k, v := range input {
+		if breakingRecords(v) != expect[k] {
+			t.Fail()
+		}
+	}
+}
+
+func breakingRecords(scores []int32) [2]int32 {
+	return nil
+}
