dfa3f4b6638748af299abb7cb1cce0f398d1438a
deChn <hey.ceth@gmail.com>
2020-03-26 23:27:45 +0100

    Very first think to use iteration.

--- birthdayChocolate_test.go
+++ birthdayChocolate_test.go
@@ -25,5 +25,41 @@ func TestBirthday(t *testing.T) {
 }
 
 func birthday(s []int32, d int32, m int32) int32 {
+
+	for k, v := s {
+		
+	}
 	return 0
+}
+
+func cut(s []int32, d int32, m int32, n int32, sum int32) bool{
+
+	if len(s) = 0 {
+		return false
+	}
+
+	if n > m {
+		return  false
+	}
+
+	for k, v := range s {
+		days := sum + v
+
+		if days > d {
+			return false
+		}
+
+		if days < d && n < m {
+			si := append([]int32(nil), s[:k], s[k+1:])
+			cut(si, d, m ,n++, days)
+		}
+		if days < d && n == m {
+			return false
+		}
+		if days == d && n == m {
+			return true
+		}
+		if n < m {
+		}
+	}
 }
