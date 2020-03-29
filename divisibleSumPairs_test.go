b259c0700df8aad5578c31ee8c3eaeb3f6b89688
deChn <hey.ceth@gmail.com>
2020-03-29 22:21:57 +0200

    Add tests. Print error.

--- divisibleSumPairs_test.go
+++ divisibleSumPairs_test.go
@@ -14,11 +14,14 @@ func TestDivisiableSumPairs(t *testing.T) {
 		{n: 6, k: 5, ar: []int32{1, 2, 3, 4, 5, 6}, expect: 3},
 		{n: 1, k: 2, ar: []int32{1}, expect: 0},
 		{n: 2, k: 2, ar: []int32{1, 1}, expect: 1},
+		{n: 3, k: 1, ar: []int32{1, 2, 3}, expect: 3},
+		{n: 3, k: 100, ar: []int32{1, 2, 3}, expect: 0},
+		{n: 3, k: 100, ar: []int32{100, 100, 100}, expect: 3},
 	}
 
 	for _, tc := range tt {
-		if divisibleSumPairs(tc.n, tc.k, tc.ar) != tc.expect {
-			t.Fail()
+		if get := divisibleSumPairs(tc.n, tc.k, tc.ar); get != tc.expect {
+			t.Errorf("Expect %v, but %v.\n", tc.expect, get)
 		}
 	}
 }
