36d78273c1190395960d7b635e45d7ad5d8d15b3
deChn <hey.ceth@gmail.com>
2020-03-29 22:13:19 +0200

    Add two more test cases.

--- divisibleSumPairs_test.go
+++ divisibleSumPairs_test.go
@@ -12,6 +12,8 @@ func TestDivisiableSumPairs(t *testing.T) {
 	tt := []testdata{
 		{n: 6, k: 3, ar: []int32{1, 3, 2, 6, 1, 2}, expect: 5},
 		{n: 6, k: 5, ar: []int32{1, 2, 3, 4, 5, 6}, expect: 3},
+		{n: 1, k: 2, ar: []int32{1}, expect: 0},
+		{n: 2, k: 2, ar: []int32{1, 1}, expect: 1},
 	}
 
 	for _, tc := range tt {
@@ -22,5 +24,15 @@ func TestDivisiableSumPairs(t *testing.T) {
 }
 
 func divisibleSumPairs(n int32, k int32, ar []int32) int32 {
-	return 0
+	pairs := 0
+
+	for index, v := range ar {
+		for scan := int32(1); scan < n-int32(index); scan++ {
+			if (v+ar[scan])%k == 0 {
+				pairs++
+			}
+		}
+	}
+
+	return int32(pairs)
 }
