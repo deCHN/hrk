fffe7a0156999f6f76c002bc5822cc8cb0bb802a
deChn <hey.ceth@gmail.com>
2020-03-31 21:46:47 +0200

    Solve day-of-programmer problem. 15/15

--- dayOfProgrammer_test.go
+++ dayOfProgrammer_test.go
@@ -13,7 +13,9 @@ func TestDayOfProgrammer(t *testing.T) {
 		{2017, "13.09.2017"}, //sample 0
 		{2016, "12.09.2016"}, //sample 1
 		{1800, "12.09.1800"}, //smaple 2
-		{2100, "13.09.2100"}, //smaple 2
+		{2100, "13.09.2100"}, //smaple 54
+		{2000, "12.09.2000"}, //smaple 58
+		{1918, "26.09.1918"}, //smaple 59
 	}
 
 	for _, v := range testdata {
@@ -26,6 +28,12 @@ func TestDayOfProgrammer(t *testing.T) {
 // Complete the dayOfProgrammer function below.
 func dayOfProgrammer(year int32) string {
 	monthday := [12]int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+
+	//The transition from the Julian to Gregorian calendar system occurred in 1918,
+	//when the next day after January 31th was February 14th.
+	if year == 1918 {
+		monthday[1] = 15
+	}
 
 	if isLeapYear(year) {
 		monthday[1] = 29
@@ -50,6 +58,9 @@ func dayOfProgrammer(year int32) string {
 // From 1700 to 1917, Russia's official calendar was the Julian calendar;
 // since 1919 they used the Gregorian calendar system.
 func isLeapYear(year int32) bool {
+	if year%400 == 0 {
+		return true
+	}
 	if year < 1919 {
 		return year%4 == 0
 	} else {
