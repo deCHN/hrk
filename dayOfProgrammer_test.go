69e562b7a96be72b6d6597b1fa11266f329a0bde
deChn <hey.ceth@gmail.com>
2020-03-31 20:48:42 +0200

    Add day-of-programmer problem.

---
+++ dayOfProgrammer_test.go
@@ -0,0 +1,13 @@
+package hrk
+
+import "testing"
+
+func TestDayOfProgrammer(t *testing.T) {
+
+}
+
+// Complete the dayOfProgrammer function below.
+func dayOfProgrammer(year int32) string {
+
+	return ""
+}
