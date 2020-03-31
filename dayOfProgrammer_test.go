99cd7f537ad8683d334ad8bf2c417093e2fa1f50
deChn <hey.ceth@gmail.com>
2020-03-31 21:00:10 +0200

    Add test.

--- dayOfProgrammer_test.go
+++ dayOfProgrammer_test.go
@@ -3,7 +3,20 @@ package hrk
 import "testing"
 
 func TestDayOfProgrammer(t *testing.T) {
+	testdata := []struct {
+		year   int32
+		expect string
+	}{
+		{2017, "13.09.2017"}, //sample 0
+		{2016, "12.09.2016"}, //sample 1
+		{1800, "12.09.1800"}, //smaple 2
+	}
 
+	for _, v := range testdata {
+		if get := dayOfProgrammer(v.year); get != v.expect {
+			t.Errorf("Expect: %s, but %s.", v.expect, get)
+		}
+	}
 }
 
 // Complete the dayOfProgrammer function below.
