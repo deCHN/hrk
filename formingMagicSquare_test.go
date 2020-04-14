dd6ef9e0aaf1b4031b4c633633fa943712557e78
deChn <hey.ceth@gmail.com>
2020-04-14 22:45:40 +0200

    A new way to cal.

--- formingMagicSquare_test.go
+++ formingMagicSquare_test.go
@@ -41,19 +41,40 @@ func TestFormingMagicSquare(t *testing.T) {
 }
 
 func formingMagicSquare(s [][]int32) int32 {
-	sum := int32(0)
-	for _, v := range s {
-		sum += row(v...)
+
+	//cover from two dimentions to one for easy calculate
+	covert := func(s [][]int32) []int32 {
+		ret := make([]int32, 9)
+		for _, v := range s {
+			ret = append(ret, v...)
+		}
+		return ret
 	}
 
-	return sum
-}
+	minCost := func(s []int32) int32 {
+		squares := [8][9]int32{
+			[9]int32{2, 9, 4, 7, 5, 3, 6, 1, 8},
+			[9]int32{4, 9, 2, 3, 5, 7, 8, 1, 6}, //mirrow of last one
+			[9]int32{6, 7, 2, 1, 5, 9, 8, 3, 4}, //rotate 90 degrees of first one
+			[9]int32{2, 7, 6, 9, 5, 1, 4, 3, 8}, //mirrow of last one
+			[9]int32{8, 1, 6, 3, 5, 7, 4, 9, 2}, //rotate 90 degrees of third one
+			[9]int32{6, 1, 8, 7, 5, 3, 2, 9, 4}, //mirrow of last one
+			[9]int32{4, 3, 8, 9, 5, 1, 2, 7, 6}, //rotate 90 degrees of third one
+			[9]int32{8, 3, 4, 1, 5, 9, 6, 7, 2}, //mirrow of last one
+		}
 
-func row(ns ...int32) int32 {
-	sum := int32(0)
-	for _, v := range ns {
-		sum += v
+		cost := float64(100)
+		for _, sqr := range squares {
+			sum := float64(0)
+			for k, v := range sqr {
+				sum += math.Abs(float64(s[k] - v))
+			}
+			if sum < cost {
+				cost = sum
+			}
+		}
+		return int32(cost)
 	}
 
-	return int32(math.Abs(float64(sum - 15)))
+	return minCost(covert(s))
 }
