d176e1dd539be6e3e7074858064a63a82a0422de
deChn <hey.ceth@gmail.com>
2020-04-16 22:36:46 +0200

    Add diff method.

--- pickingNumbers_test.go
+++ pickingNumbers_test.go
@@ -1,6 +1,9 @@
 package hrk_test
 
-import "testing"
+import (
+	"sort"
+	"testing"
+)
 
 func TestPickingNumbers(t *testing.T) {
 	tests := []struct {
@@ -19,5 +22,32 @@ func TestPickingNumbers(t *testing.T) {
 }
 
 func pickingNumbers(a []int32) int32 {
-	return 0
+	max := 0
+
+	//{4, 6, 5, 3, 3, 1},
+	sort.Slice(a, func(i int, j int) bool { return a[i] < a[j] })
+	//{1, 3, 3, 4, 5, 6},
+
+	//size := len(a)
+	//n := 0
+
+	diff := func(s []int32, v0 int32) int {
+		for k, v := range s {
+			s[k] = v - v0
+		}
+		//v0 = 3 -> {-2, 0, 0, 1, 2, 3},
+		//TODO: find first index of -1 or 0
+		//TODO: find last index of 0 or 1
+
+		return 0
+	}
+
+	// {1,2
+	for _, v0 := range a {
+		s := make([]int32, 0)
+		copy(s, a)
+		diff(s, v0)
+	}
+
+	return int32(max)
 }
