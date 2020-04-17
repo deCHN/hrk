ae1e691ac57cef6d4867eef735fb3354ef886f05
deChn <hey.ceth@gmail.com>
2020-04-17 20:25:39 +0200

    Add testcase.

--- pickingNumbers_test.go
+++ pickingNumbers_test.go
@@ -12,6 +12,7 @@ func TestPickingNumbers(t *testing.T) {
 	}{
 		{[]int32{4, 6, 5, 3, 3, 1}, 3},
 		{[]int32{1, 2, 2, 3, 1, 2}, 5},
+		{[]int32{6, 6, 6, 6, 6, 6}, 6}, // testcase 6
 	}
 
 	for _, v := range tests {
@@ -28,15 +29,12 @@ func pickingNumbers(a []int32) int32 {
 	sort.Slice(a, func(i int, j int) bool { return a[i] < a[j] })
 	//{1, 3, 3, 4, 5, 6},
 
-	//size := len(a)
-	//n := 0
-
 	diff := func(s []int32, v0 int32) int {
 		for k, v := range s {
 			s[k] = v - v0
 		} //v0 = 3 -> {-2, 0, 0, 1, 2, 3},
 
-		il, ir, left := len(s), 0, int32(0) //index left / right
+		il, ir, left := len(s), 0, int32(0) //index left / right, left(min) value of the set
 		for k, v := range s {
 			if v >= -1 {
 				il = k
@@ -46,7 +44,7 @@ func pickingNumbers(a []int32) int32 {
 		} // il = 1
 
 		for k, v := range s {
-			if v < left+2 {
+			if v < left+2 { // left+2 <- right(max) value of the set
 				continue
 			}
 			ir = k - 1
