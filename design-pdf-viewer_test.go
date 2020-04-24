d35befcceabc3390ef00f682359d38c75f05ce96
deChn <hey.ceth@gmail.com>
2020-04-24 20:28:29 +0200

    Add test case.

---
+++ design-pdf-viewer_test.go
@@ -0,0 +1,25 @@
+package hrk_test
+
+import "testing"
+
+func TestDesignerPdfViewer(t *testing.T) {
+	tests := []struct {
+		h    []int32
+		word string
+		want int32
+	}{
+		{[]int32{1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5}, "abc", 9},
+		{[]int32{1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7}, "zaba", 28},
+	}
+
+	for _, v := range tests {
+		if get := designerPdfViewer(v.h, v.word); get != v.want {
+			t.Errorf("Given %v, want %v, but get %v.\n", v.word, v.want, get)
+		}
+	}
+}
+
+// https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
+func designerPdfViewer(h []int32, word string) int32 {
+	return 0
+}
