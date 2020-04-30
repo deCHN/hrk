1ff08443311bf5c29b844dff2931bc9f83d6d3b3
deChn <hey.ceth@gmail.com>
2020-04-30 19:08:13 +0200

    Add more test data.

--- strangeAdvertising_test.go
+++ strangeAdvertising_test.go
@@ -6,7 +6,10 @@ func TestViralAdvertising(t *testing.T) {
 	tests := []struct {
 		n, want int32
 	}{
+		{1, 2},
 		{3, 9},
+		{4, 15},
+		{5, 24},
 	}
 
 	for _, v := range tests {
@@ -17,6 +20,16 @@ func TestViralAdvertising(t *testing.T) {
 
 }
 
+/*
+* https://www.hackerrank.com/challenges/strange-advertising/problem
+*
+* Day Shared Liked Cumulative
+* 1      5     2       2
+* 2      6     3       5
+* 3      9     4       9
+* 4     12     6      15
+* 5     18     9      24
+ */
 func viralAdvertising(n int32) int32 {
 	return 0
 }
