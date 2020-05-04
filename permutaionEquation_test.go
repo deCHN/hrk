920365195cff3a09c7aed475c279c7da1b37d6d8
deChn <hey.ceth@gmail.com>
2020-05-04 11:02:54 +0200

    Correct test code.

--- permutaionEquation_test.go
+++ permutaionEquation_test.go
@@ -6,19 +6,20 @@ func TestPermutationEquation(t *testing.T) {
 	tests := []struct {
 		p, want []int32
 	}{
-		{[]int32{}, []int32{1}},
+		{[]int32{2, 3, 1}, []int32{2, 3, 1}},
 	}
 
 	for _, v := range tests {
 		get := permutationEquation(v.p)
 		for k, item := range v.want {
-			if get[k] != item {
-				t.Errorf("Given %v, want %v, but get %v.\n", v.p[k], item, get[k])
+			if len(get) <= k || get[k] != item {
+				t.Errorf("Given %v, want %v, but get %v.\n", v.p, v.want, get)
+				break
 			}
 		}
 	}
 }
 
 func permutationEquation(p []int32) []int32 {
-	return nil
+	return []int32{}
 }
