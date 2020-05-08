6655fa6183e7f7a63b723ad04f9caab07f2709b5
deChn <hey.ceth@gmail.com>
2020-05-08 12:56:33 +0200

    Add jumping-on-cloud problem.

---
+++ jumpingOnTheCloudsRevisited_test.go
@@ -0,0 +1,23 @@
+package hrk_test
+
+import "testing"
+
+func TestJumpingOnClouds(t *testing.T) {
+	tests := []struct {
+		c       []int32
+		k, want int32
+	}{
+		{[]int32{0, 0, 1, 0, 0, 1, 1, 0}, 2, 92},
+	}
+
+	for _, v := range tests {
+		if get := jumpingOnClouds(v.c, v.k); get != v.want {
+			t.Errorf("Given %v, want %v, but get %v.\n", v.c, v.want, get)
+		}
+	}
+
+}
+
+func jumpingOnClouds(c []int32, k int32) int32 {
+	return 0
+}
