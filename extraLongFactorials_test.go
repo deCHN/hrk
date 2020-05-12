7f5845413b1ad06ec96d917502f5700a35e21b26
deChn <hey.ceth@gmail.com>
2020-05-12 18:33:38 +0200

    Solve extra-long-factorials problem. 20/20

--- extraLongFactorials_test.go
+++ extraLongFactorials_test.go
@@ -1,6 +1,26 @@
 package hrk_test
 
-// https://www.hackerrank.com/challenges/extra-long-factorials/problem
-func extraLongFactorials(n int32) {
+import (
+	"math/big"
+	"testing"
+)
 
+func TestExtraLongFactorials(t *testing.T) {
+	tests := []struct {
+		n    int32
+		want string
+	}{
+		{25, "15511210043330985984000000"},
+	}
+
+	for _, v := range tests {
+		if get := extraLongFactorials(v.n); get != v.want {
+			t.Errorf("Given %v, want %v, but get %v.\n", v.n, v.want, get)
+		}
+	}
+}
+
+// https://www.hackerrank.com/challenges/extra-long-factorials/problem
+func extraLongFactorials(n int32) string {
+	return big.NewInt(0).MulRange(1, int64(n)).String()
 }
