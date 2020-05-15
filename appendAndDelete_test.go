203842913e7701e28ef4ee794898dd42f746ccb6
deChn <hey.ceth@gmail.com>
2020-05-15 22:53:48 +0200

    First implement passes 10/14 tests.

--- appendAndDelete_test.go
+++ appendAndDelete_test.go
@@ -1,14 +1,18 @@
 package hrk_test
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestAppendAndDelete(t *testing.T) {
 	tests := []struct {
 		s, t, want string
 		k          int32
 	}{
-		{s: "aba", t: "aba", k: 7, want: "yes"},
-		{s: "hackerhappy", t: "hackerrank", k: 9, want: "yes"},
+		//{s: "aba", t: "aba", k: 7, want: "Yes"},
+		//{s: "hackerhappy", t: "hackerrank", k: 9, want: "Yes"},
+		//{s: "ashley", t: "ash", k: 2, want: "No"},
+		{s: "aaaaaaaaaa", t: "aaaaa", k: 7, want: "Yes"},
 	}
 
 	for _, v := range tests {
@@ -20,6 +24,29 @@ func TestAppendAndDelete(t *testing.T) {
 
 // https://www.hackerrank.com/challenges/append-and-delete/problem
 func appendAndDelete(s string, t string, k int32) string {
+	var diff int
 
-	return ""
+	// s: abci -> len(s) - k
+	// t: abcde  -> k = 3 -> len(t) - k
+	for k, v := range t {
+		if k == len(s) {
+			diff = k
+			break
+		}
+		if byte(v) != s[k] {
+			diff = k
+			break
+		}
+	}
+
+	del := len(s) - diff
+	app := len(t) - diff
+
+	//fmt.Printf("Del: %v, Append: %v.\n", del, app)
+
+	if (del + app) <= int(k) {
+		return "Yes"
+	}
+
+	return "No"
 }
