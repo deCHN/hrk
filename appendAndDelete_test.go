e59fde60de10fb2efc51f96e3aa4342c78bb7b1f
deChn <hey.ceth@gmail.com>
2020-05-15 22:24:23 +0200

    Add test cases 0, 1.

--- appendAndDelete_test.go
+++ appendAndDelete_test.go
@@ -1,6 +1,25 @@
 package hrk_test
 
+import "testing"
+
+func TestAppendAndDelete(t *testing.T) {
+	tests := []struct {
+		s, t, want string
+		k          int32
+	}{
+		{s: "aba", t: "aba", k: 7, want: "yes"},
+		{s: "hackerhappy", t: "hackerrank", k: 9, want: "yes"},
+	}
+
+	for _, v := range tests {
+		if get := appendAndDelete(v.s, v.t, v.k); get != v.want {
+			t.Errorf("Given %v, want %v, but get %v.\n", v, v.want, get)
+		}
+	}
+}
+
 // https://www.hackerrank.com/challenges/append-and-delete/problem
 func appendAndDelete(s string, t string, k int32) string {
 
+	return ""
 }
