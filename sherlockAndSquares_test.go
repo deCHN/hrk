39cc395ca88fc611f342bf4ce557472b3e12de93
deChn <hey.ceth@gmail.com>
2020-05-19 11:41:10 +0200

    Add basic test cases.

--- sherlockAndSquares_test.go
+++ sherlockAndSquares_test.go
@@ -1,5 +1,23 @@
 package hrk_test
 
-func squares(a int32, b int32) int32 {
+import "testing"
 
+func TestSquares(t *testing.T) {
+	tests := []struct {
+		a, b, want int32
+	}{
+		{3, 9, 2},
+		{17, 24, 0},
+	}
+
+	for _, v := range tests {
+		if get := squares(v.a, v.b); get != v.want {
+			t.Errorf("Given %v, want %v, but get %v.\n", v, v.want, get)
+		}
+	}
+
+}
+
+func squares(a int32, b int32) int32 {
+	return 0
 }
