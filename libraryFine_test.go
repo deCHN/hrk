dba7ad57a3b7cd7f36cd61a000844c305b39c82f
deChn <hey.ceth@gmail.com>
2020-05-20 23:12:06 +0200

    Solve problem. 15/15

--- libraryFine_test.go
+++ libraryFine_test.go
@@ -7,6 +7,8 @@ func TestLibraryFine(t *testing.T) {
 		d1, m1, y1, d2, m2, y2, want int32
 	}{
 		{9, 6, 2015, 6, 6, 2015, 45},
+		{6, 6, 2015, 9, 6, 2016, 0},
+		{2, 7, 1014, 1, 1, 1015, 0},
 	}
 
 	for _, v := range tests {
@@ -21,13 +23,22 @@ func TestLibraryFine(t *testing.T) {
 // d1, m1, y1: returned date day, month and year
 // d2, m2, y2: due date day, month and year
 func libraryFine(d1 int32, m1 int32, y1 int32, d2 int32, m2 int32, y2 int32) int32 {
-	if y1-y2 > 0 {
+
+	if y := y1 - y2; y > 0 {
 		return 10000
+	} else if y < 0 {
+		return 0
 	}
 
 	if m := m1 - m2; m > 0 {
 		return m * 500
+	} else if m < 0 {
+		return 0
 	}
 
-	return (d1 - d2) * 15
+	if d := d1 - d2; d > 0 {
+		return d * 15
+	}
+
+	return 0
 }
