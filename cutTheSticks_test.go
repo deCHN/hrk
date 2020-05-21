b917c5aaf3178166236a81de51ed0073de117364
deChn <hey.ceth@gmail.com>
2020-05-21 21:28:22 +0200

    Add complare slice method.

--- cutTheSticks_test.go
+++ cutTheSticks_test.go
@@ -1,6 +1,27 @@
 package hrk_test
 
+import "testing"
+
+func TestCutTheSticks(t *testing.T) {
+	tests := []struct {
+		arr, want []int32
+	}{
+		{[]int32{1, 2, 3, 4, 3, 3, 2, 1}, []int32{8, 6, 4, 1}},
+	}
+
+	for _, v := range tests {
+		get := cutTheSticks(v.arr)
+		if ok, err := compareSlice(v.want, get); !ok || err != nil {
+			t.Errorf("Given %v, want %v, but get %v.\n", v.arr, v.want, get)
+		}
+	}
+}
+
 // https://www.hackerrank.com/challenges/cut-the-sticks/problem
 func cutTheSticks(arr []int32) []int32 {
-	return nil
+	return []int32{}
+}
+
+func compareSlice(a, b []int32) (bool, error) {
+
 }
