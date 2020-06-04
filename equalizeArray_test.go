0f128ab5a5ecb114fc7cc7dcb210154484a4c840
deChn <hey.ceth@gmail.com>
2020-06-05 00:49:15 +0200

    Add equalize-array problem. 20/20

---
+++ equalizeArray_test.go
@@ -0,0 +1,23 @@
+package hrk_test
+
+import "testing"
+
+func TestEqualizeArray(t *testing.T) {
+	tests := []struct {
+		arr  []int32
+		want int32
+	}{
+		{[]int32{1, 2, 2, 3}, 2},
+		{[]int32{3, 3, 2, 1, 3}, 2},
+	}
+
+	for _, v := range tests {
+		if get := equalizeArray(v.arr); get != v.want {
+			t.Errorf("Given %v, want %v, but get %v.\n", v, v.want, get)
+		}
+	}
+}
+
+func equalizeArray(arr []int32) int32 {
+	return 0
+}
