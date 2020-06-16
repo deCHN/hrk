95731f9337ff493e5eaae7e832b9db973274ccbc
deChn <hey.ceth@gmail.com>
2020-06-16 22:51:41 +0200

    Add test.

--- acmIcpcTeam_test.go
+++ acmIcpcTeam_test.go
@@ -9,9 +9,24 @@ func TestAcmTeam(t *testing.T) {
 	}{
 		{[]string{"10101", "11110", "00010"}, []int32{5, 1}},
 	}
+
+	for _, v := range tests {
+		if get := acmTeam(v.topic); get != nil {
+			if len(get) != len(v.want) {
+				t.Errorf("Size not the same. Want %v, but get %v.\n", len(v.want), len(get))
+			}
+			for k, w := range get {
+				if w != v.want[k] {
+					t.Errorf("Given %v, want %v, but get %v.\n", v, v.want[k], w)
+				}
+			}
+		} else {
+			t.Errorf("Failed. nil returned.\n")
+		}
+	}
 }
 
 // https://www.hackerrank.com/challenges/acm-icpc-team/problem
 func acmTeam(topic []string) []int32 {
-
+	return nil
 }
