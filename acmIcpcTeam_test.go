e973cb48aa946dadaab822eec957f60368e1b605
deChn <hey.ceth@gmail.com>
2020-09-16 10:37:43 +0200

    2 tests passes.

--- acmIcpcTeam_test.go
+++ acmIcpcTeam_test.go
@@ -8,6 +8,7 @@ func TestAcmTeam(t *testing.T) {
 		want  []int32
 	}{
 		{[]string{"10101", "11110", "00010"}, []int32{5, 1}},
+		{[]string{"10101", "11100", "11010", "00101"}, []int32{5, 2}},
 	}
 
 	for _, v := range tests {
@@ -28,5 +29,21 @@ func TestAcmTeam(t *testing.T) {
 
 // https://www.hackerrank.com/challenges/acm-icpc-team/problem
 func acmTeam(topic []string) []int32 {
-	return nil
+	max, n := int32(0), int32(0)
+	for k, v := range topic {
+		for i := k + 1; i < len(topic); i++ {
+			set := int32(0)
+			for skill := range v {
+				if v[skill] == 49 || topic[i][skill] == 49 { // ASCII(49) -> 1
+					set++
+				}
+			}
+			if set > max {
+				max = set
+				n++
+			}
+		}
+	}
+
+	return []int32{max, n}
 }
