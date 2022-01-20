2c5d465e94bfd8dab3cacf9eafd7d45ec13eb74d
deChn <hey.ceth@gmail.com>
2022-01-20 21:51:46 +0100

    func: Problem solved 25/25.

--- sparseArray_test.go
+++ sparseArray_test.go
@@ -11,16 +11,33 @@ func TestMatchingStrings(t *testing.T) {
 		in   [2][]string
 		want []int32
 	}{
-		{[2][]string{[]string{"aba", "baba", "aba", "xzxb"}, []string{"aba", "xzxb", "ab"}}, []int32{2, 1, 0}},
-		{[2][]string{[]string{"def", "de", "fgh"}, []string{"de", "lmn", "fgh"}}, []int32{1, 0, 1}},
+		{ //1
+			[2][]string{
+				[]string{"aba", "baba", "aba", "xzxb"},
+				[]string{"aba", "xzxb", "ab"}},
+			[]int32{2, 1, 0},
+		},
+		{ //2
+			[2][]string{
+				[]string{"def", "de", "fgh"},
+				[]string{"de", "lmn", "fgh"}},
+			[]int32{1, 0, 1},
+		},
+		{ //3
+			[2][]string{
+				[]string{"abcde", "sdaklfj", "asdjf", "na", "basdn", "sdaklfj", "asdjf", "na", "asdjf", "na", "basdn", "sdaklfj", "asdjf"},
+				[]string{"abcde", "sdaklfj", "asdjf", "na", "basdn"}},
+
+			[]int32{1, 3, 4, 3, 2},
+		},
 	}
 
-	for _, tc := range tcs {
+	for n, tc := range tcs {
 		get := matchingStrings(tc.in[0], tc.in[1])
 
 		for k, v := range tc.want {
 			if get[k] != v {
-				t.Errorf("Want: %v, but get: %v.", v, get[k])
+				t.Errorf("Test case %v failed. Want \"%s\": %v, but get: %v.", n+1, tc.in[1][k], v, get[k])
 			}
 		}
 	}
