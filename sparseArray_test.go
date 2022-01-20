24fcd93dfdd2e341082d122730af5cff6e599f76
deChn <hey.ceth@gmail.com>
2022-01-20 15:59:14 +0100

    doc: add challenge.

---
+++ sparseArray_test.go
@@ -0,0 +1,11 @@
+package hrk
+
+/*
+ * There is a collection of input strings and a collection of query strings. For each query string, determine how many times it occurs in the list of input strings. Return an array of the results.
+ *
+ */
+
+func matchingStrings(strings []string, queries []string) []int32 {
+	// Write your code here
+	return nil
+}
