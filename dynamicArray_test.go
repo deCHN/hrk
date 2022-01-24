caa36f7d93f020788565e91c74da4f4e9c527e3f
deChn <hey.ceth@gmail.com>
2022-01-24 21:13:33 +0100

    func: first implementation. Solve the first example.

--- dynamicArray_test.go
+++ dynamicArray_test.go
@@ -1,12 +1,27 @@
 package hrk
 
-func TestDynamicArray(t *tesitng.T) {
+import "testing"
+
+func TestDynamicArray(t *testing.T) {
 	td := []struct {
 		n       int32
 		queries [][]int32
 		want    []int32
 	}{
 		{2, [][]int32{{1, 0, 5}, {1, 1, 7}, {1, 0, 3}, {2, 1, 0}, {2, 1, 1}}, []int32{7, 3}},
+	}
+
+	for k, tc := range td {
+		get := dynamicArray(tc.n, tc.queries)
+
+		if len(get) != len(tc.want) {
+			t.Errorf("Test %d failed. Expected results size is %d, but get %d.", k, tc.want, len(get))
+		}
+		for i, v := range get {
+			if v != tc.want[i] {
+				t.Errorf("Test %d failed at %d-th element. Exepect array %v, but get %v.", k, i, tc.want, get)
+			}
+		}
 	}
 }
 
@@ -44,5 +59,22 @@ func TestDynamicArray(t *tesitng.T) {
  * Each of the  subsequent lines contains a query string,
  */
 func dynamicArray(n int32, queries [][]int32) []int32 {
-	// Write your code here
+	arr := make([][]int32, n)
+	r := make([]int32, 0)
+	lastAnswer := int32(0)
+	idx := int32(0)
+
+	for _, q := range queries {
+		idx = q[1] ^ lastAnswer%n
+		if q[0] == int32(1) {
+			arr[idx] = append(arr[idx], q[2])
+		} else if q[0] == 2 {
+			lastAnswer = arr[idx][q[2]%int32(len(arr[idx]))]
+			r = append(r, lastAnswer)
+		} else {
+			return nil
+		}
+	}
+
+	return r
 }
