2a98e5d3b1fcfe339599a39b858f2d998579e603
deChn <hey.ceth@gmail.com>
2022-01-26 09:07:01 +0100

    test: add challenge. 0/20

---
+++ maximumElement_test.go
@@ -0,0 +1,43 @@
+package hrk
+
+/*
+ * https://www.hackerrank.com/challenges/maximum-element/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+ * You have an empty sequence, and you will be given  queries. Each query is one of these three types:
+ *
+ * 1 x  -Push the element x into the stack.
+ * 2    -Delete the element present at the top of the stack.
+ * 3    -Print the maximum element in the stack.
+ * Function Description
+ *
+ * Complete the getMax function in the editor below.
+ *
+ * getMax has the following parameters:
+ * - string operations[n]: operations as strings
+ *
+ * Returns
+ * - int[]: the answers to each type 3 query
+ *
+ * Input Format
+ *
+ * The first line of input contains an integer, . The next  lines each contain an above mentioned query.
+ *
+ * Constraints
+ *
+ * Constraints
+ *
+ *
+ *
+ * All queries are valid.
+ */
+
+/*
+ * Complete the 'getMax' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts STRING_ARRAY operations as parameter.
+ */
+
+func getMax(operations []string) []int32 {
+	// Write your code here
+	return nil
+}
