0ba3aaa4864b8ca6820a57e7d42f710a2a4c211c
deChn <hey.ceth@gmail.com>
2022-01-29 19:24:22 +0100

    test: read inputs for test.

--- balanced-brackets/balancedBrackets_test.go
+++ balanced-brackets/balancedBrackets_test.go
@@ -1,6 +1,12 @@
 package hrk
 
 import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -90,13 +96,42 @@ func isBalanced(s string) string {
 	return "YES"
 }
 
-//func isPair(p rune, q rune) bool {
-//if p == '(' && q == ')' {
-//return true
-//} else if p == '[' && q == ']' {
-//return true
-//} else if p == '{' && q == '}' {
-//return true
-//}
-//return false
-//}
+func TestIsBalancedInputs(t *testing.T) {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	tTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	t := int32(tTemp)
+
+	for tItr := 0; tItr < int(t); tItr++ {
+		s := readLine(reader)
+
+		result := isBalanced(s)
+
+		fmt.Fprintf(writer, "%s\n", result)
+	}
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
