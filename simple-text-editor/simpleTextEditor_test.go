7707867e3b3c7dbc28724e8a436cd900919ef7db
deChn <hey.ceth@gmail.com>
2022-02-01 16:11:42 +0100

    doc: add challenge.

---
+++ simple-text-editor/simpleTextEditor_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+/*
+ * Implement a simple text editor. The editor initially contains an empty string, . Perform  operations of the following  types:
+ *
+ * 1 - append - Append string  to the end of .
+ * 2 - delete - Delete the last  characters of .
+ * 3 - print - Print the  character of .
+ * 4 - undo - Undo the last (not previously undone) operation of type  or , reverting  to the state it was in prior to that operation.
+ */
+func TestHelloWorld(t *testing.T) {
+
+}
