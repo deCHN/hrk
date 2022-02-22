93bff9f10d5f3b85b5a68024ee21e7b26acdcdfd
deChn <hey.ceth@gmail.com>
2022-02-22 21:23:11 +0100

    doc: add challenge QHEAP1.

---
+++ QHEAP1_test.go
@@ -0,0 +1,26 @@
+package hrk
+
+import "testing"
+
+/*
+ * https://www.hackerrank.com/challenges/qheap1/problem
+ *
+ * This question is designed to help you get a better understanding of basic heap operations.
+ *
+ * There are 3 types of query:
+ *
+ * "1 v " - Add an element v to the heap.
+ * "2 v " - Delete the element v from the heap.
+ * "3" - Print the minimum of all the elements in the heap.
+ *
+ * NOTE: It is guaranteed that the element to be deleted will be there in the heap. Also, at any instant, only distinct elements will be in the heap.
+ *
+ * Input Format
+ *
+ * The first line contains the number of queries, Q.
+ * Each of the next Q lines contains one of the 3 types of query.
+ */
+
+func TestQHEAP1(t *testing.T) {
+
+}
